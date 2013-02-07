820d87a8ce3e965a52bdae597ef525e2c48811a6
Antoine Rouaze <antoine.rouaze@zenika.com>
2013-02-07 14:54:07 +0100

    Add correction n°3.

---
+++ src/com/supinfo/suprails/entity/FreightTrain.java
@@ -0,0 +1,42 @@
+package com.supinfo.suprails.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+/**
+ * @author Antoine Rouaze <antoine.rouaze@zenika.com>
+ */
+@Entity
+@DiscriminatorValue("FreightTrain")
+public class FreightTrain extends Train {
+
+    private Long maxWeight;
+
+    public Long getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(Long maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        FreightTrain that = (FreightTrain) o;
+
+        if (maxWeight != null ? !maxWeight.equals(that.maxWeight) : that.maxWeight != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (maxWeight != null ? maxWeight.hashCode() : 0);
+        return result;
+    }
+}
